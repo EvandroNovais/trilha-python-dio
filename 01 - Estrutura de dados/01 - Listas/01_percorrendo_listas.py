1192ad73001c9f40059d20f5eed14da76bedc325
EvandroNovais <evandro-novais@outlook.com>
2023-07-21 09:14:31 -0300

    Percorrendo listas

---
+++ 01 - Estrutura de dados/01 - Listas/01_percorrendo_listas.py
@@ -0,0 +1,8 @@
+lista = ["p","y","t","h","o","n"]
+
+print(lista[2:]) # tudo após o indice 2
+print(lista[:2]) # tudo antes do indice 2
+print(lista[1:3]) # inicio:fim
+print(lista[0:3:2])# inicio:fim:step
+print(lista[::]) # Padrão (Imprime tudo)
+print(lista[::-1]) # Imprime tudo ao contrário
