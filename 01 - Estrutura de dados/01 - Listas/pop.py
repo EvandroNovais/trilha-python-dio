11686fe970026dee2e448103a88ffe0bc1d07431
EvandroNovais <evandro-novais@outlook.com>
2023-11-09 14:22:53 -0300

    pop

---
+++ 01 - Estrutura de dados/01 - Listas/pop.py
@@ -0,0 +1,8 @@
+# Recupera cada elemento de uma lista do ultimo para o primeiro
+# Conceito de pilha.
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.pop())
+print(linguagens.pop())
+print(linguagens.pop())
+print(linguagens.pop(0)) # Retira um elemento especifico da pilha.
